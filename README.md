1998c3caa8f5c85c31f4b6471d3a03629ff5482f
aceDavon <makojidavid@gmail.com>
2022-10-12 11:08:27 +0100

    Create challenge code
    Create and implement the challeng code to fix the problem statement

---
+++ README.md
@@ -0,0 +1,2 @@
+# Multiple Pointers
+This repo is part of a group of repos created to practice Algorithm and Data structures with automated workflows and tests
---
+++ challenge.js
@@ -0,0 +1,23 @@
+const pointer = (arr) => {
+  // Initialize multi pointers at different 
+  // points in the array
+  let left = 0;
+  let right = arr.length -1;
+
+  // Loop over array and sum up each pointer
+  while(left < right) {
+    const sum = arr[left] + arr[right];
+    // if sum equals 0, breakout
+    if(sum === 0) return [arr[left], arr[right]];
+
+    // if sum is greater than 0, mover the right 
+    // pointer to a lesser number down the array
+    if(sum > 0) right -=1;
+
+    // if sum id less than 0, move the left pointer
+    // to a bigger number up the array
+    if(sum < 0) left +=1;
+  }
+};
+
+console.log(pointer([-2, -1, 0, 6, 10]));
